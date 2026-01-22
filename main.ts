fe5c78c6ce19cc33714b667ce4edff1cbc5b035c
okp515809-create <okp515809@gnspes.ca>
2026-01-22 14:14:49 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+namespace SpriteKind {
+    export const Decoration = SpriteKind.create()
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`swim left`,
+    200,
+    true
+    )
+})
+info.onCountdownEnd(function () {
+    game.over(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 100)
+    info.changeScoreBy(1)
+})
+let myFood: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+scene.setBackgroundImage(assets.image`ocean1`)
+mySprite = sprites.create(assets.image`shark`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+info.startCountdown(15)
+for (let index = 0; index < 10; index++) {
+	
+}
+let myDecor = sprites.create(assets.image`decoration`, SpriteKind.Decoration)
+myDecor.setPosition(randint(0, 160), 96)
+animation.runImageAnimation(
+mySprite,
+assets.animation`swim right`,
+200,
+true
+)
+game.onUpdateInterval(2100, function () {
+    myFood = sprites.create(assets.image`food`, SpriteKind.Food)
+    myFood.setPosition(scene.screenWidth(), randint(5, 115))
+    myFood.vx = -75
+})
